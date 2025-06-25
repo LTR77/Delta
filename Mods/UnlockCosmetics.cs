84b2153004130beddb2f23deb2fec3c2e39dc9ab
LTR77 <linusrhone77@gmaill.com>
2025-06-25 13:54:27 +0200

    UNLOCK COSMETICS

---
+++ Mods/UnlockCosmetics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using GorillaNetworking;
+
+namespace GorillaX.Mods
+{
+    internal class UnlockCosmetics
+    {
+        public static void UnlockCosmeticsMod()
+        {
+            if (CosmeticsController.instance == null) return;
+            foreach(CosmeticsController.CosmeticItem cosmetic in CosmeticsController.instance.allCosmetics)
+            {
+                CosmeticsController.instance.unlockedCosmetics.Add(cosmetic);
+                string itemName = cosmetic.itemName;
+                CosmeticsController.instance.concatStringCosmeticsAllowed += itemName + ",";
+            }
+        }
+    }
+}
