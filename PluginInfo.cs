ee4d272c812a4c8d8ffedb622e194aa7a09df856
LTR77 <linusrhone77@gmail.com>
2025-07-05 13:17:05 -0400

    name

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -3,8 +3,8 @@
     internal class PluginInfo
     {
         public const string GUID = "org.m.gorillatag.menu.GorillaX";
-        public const string Name = "Ǥㄖ尺ⅼ⎳⎳ᗩ☓✌︎";
-        // public const string Name = "GorillaX";
+        // public const string Name = "Ǥㄖ尺ⅼ⎳⎳ᗩ☓✌︎";
+        public const string Name = "GorillaX";
         public const string Description = "Created by @✞ B135 ✞ and @Royal999 with love <3";
         public const string Version = "2.5.0";
     }
