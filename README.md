dc2f717c78319a5610f1ac8c5d166456cf685145
LTR77 <linusrhone77@gmail.com>
2025-09-20 20:29:27 +0200

    delete branch

--- README.md
+++
@@ -1,6 +0,0 @@
-# ᗫ乇ㄥ♰ᗩ Mod Menu 
-https://discord.gg/tHFpuRpk
-
-### A Free and Open-Source Gorilla Tag Mod Menu
-
-
--- build.sh
+++
@@ -1,2 +0,0 @@
-dotnet build Delta.csproj -c Release
-mv bin/Release/netstandard2.1/Delta.dll /storage/emulated/0/Download/
